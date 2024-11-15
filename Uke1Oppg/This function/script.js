6415864365ed354a4fd68a57258578d377e9c6f9
Mathias Jacobsen <75167111+MathiasRepo@users.noreply.github.com>
2024-11-15 13:44:46 +0100

    Changed stuff

---
+++ Uke1Oppg/This function/script.js
@@ -0,0 +1,11 @@
+const input = document.getElementById("textInput");
+const output = document.getElementById("outputDiv");
+
+function printText() {
+    const input = document.getElementById("textInput").value;
+
+    output.innerHTML += /*HTML*/ `
+        <li><input type="checkbox">${input}</li>
+    `;
+    
+}
